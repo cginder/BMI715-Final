3512b38ab8b1f20324a58f6fa32ef8f879d6c8eb
cginder <35152229+cginder@users.noreply.github.com>
2023-12-14 16:42:47 -0500

    Update Final Project.R

--- Final Project.R
+++ Final Project.R
@@ -21,8 +21,19 @@ variables <- c(  "X", #Respondent ID
 
 df <- filtered_df %>% select(any_of(variables))
 
-#
+#Exploratory Analysis/QC
+df %>% select(-c(X,LBXAPB)) %>% plot()
+df %>% select(-c(X,LBXAPB)) %>% cor()
 
+#MCQ370A and MCQ370B highly correlated at 0.47, remove MCQ370B
+#MCQ370A and BMI negatively correlated at -.23, keep
+#Triglycerides and TC correlated at 0.35, keep due to pathophysiologic plausability
+#HUQ010 and HUQ051 correlated at 0.22, keep
+df <- df %>% select(-MCQ370B)
 
+#Remove Outliers in LBXTC and LBXSTR
+head(sort(df$LBXTC,decreasing=T))
+head(sort(df$LBXSTR,decreasing=T))
 
-plot(df[,-c(1:2)]) #excluding PatientID & ApoB
+df <- df %>% filter(LBXTC != 612 | LBXSTR != 6057)
+     
